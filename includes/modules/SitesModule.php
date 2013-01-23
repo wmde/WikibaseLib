wikimedia/mediawiki-extensions-Wikibase

Make use of new sites code in core
Change-Id: Ie639247b59abce24c9c43420de8a642e63f0978b

lib/includes/modules/SitesModule.php
@@ -50,7 +50,7 @@ class SitesModule extends ResourceLoaderModule {
 		/**
 		 * @var MediaWikiSite $site
 		 */
-		foreach ( Sites::singleton()->getSites() as $site ) {
+		foreach ( \SitesTable::newInstance()->getSites() as $site ) {
 			if ( $site->getType() === Site::TYPE_MEDIAWIKI && $site->getGroup() === 'wikipedia' ) {
 				$languageName = Utils::fetchLanguageName( $site->getLanguageCode() );
 

lib/tests/phpunit/item/TestItems.php
@@ -58,7 +58,7 @@ final class TestItems {
 		$item = Item::newEmpty();
 
 		$group = \Wikibase\Settings::get( 'siteLinkGroup' );
-		$sites = \Sites::singleton()->getSiteGroup( $group );
+		$sites = \SitesTable::newInstance()->getSites()->getGroup( $group );
 
 		if ( count( $sites ) > 1 ) {
 			$item->addSiteLink( new SiteLink( $sites->getIterator()->current(), 'spam' ) );

wmde/WikibaseLib

includes/modules/SitesModule.php
@@ -50,7 +50,7 @@ class SitesModule extends ResourceLoaderModule {
 		/**
 		 * @var MediaWikiSite $site
 		 */
-		foreach ( Sites::singleton()->getSites() as $site ) {
+		foreach ( \SiteSQLStore::newInstance()->getSites() as $site ) {
 			if ( $site->getType() === Site::TYPE_MEDIAWIKI && $site->getGroup() === 'wikipedia' ) {
 				$languageName = Utils::fetchLanguageName( $site->getLanguageCode() );
 

tests/phpunit/item/TestItems.php
@@ -58,7 +58,7 @@ final class TestItems {
 		$item = Item::newEmpty();
 
 		$group = \Wikibase\Settings::get( 'siteLinkGroup' );
-		$sites = \Sites::singleton()->getSiteGroup( $group );
+		$sites = \SiteSQLStore::newInstance()->getSites()->getGroup( $group );
 
 		if ( count( $sites ) > 1 ) {
 			$item->addSiteLink( new SiteLink( $sites->getIterator()->current(), 'spam' ) );