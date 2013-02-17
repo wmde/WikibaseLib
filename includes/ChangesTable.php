wikimedia/mediawiki-extensions-Wikibase

(bug 43990) Robust serialization of change objects
This change causes Diffs contained in Change objects to be serialized
as JSON structures instead of objects using PHP serialization. This
allows for more flexibility in parsing, and avoids problems when changing
the structure of the diff representation.

Change-Id: I9fd62298f6176f3937850cc3e009b22fa7126826

lib/includes/ChangesTable.php
@@ -85,7 +85,7 @@ class ChangesTable extends \ORMTable {
 
 			'type' => 'str',
 			'time' => 'str', // TS_MW
-			'info' => 'blob',
+			'info' => 'json', // handled specially by ChangeRow
 			'object_id' => 'str',
 			'user_id' => 'int',
 			'revision_id' => 'int',

wmde/WikibaseLib

includes/ChangesTable.php
@@ -85,7 +85,7 @@ class ChangesTable extends \ORMTable {
 
 			'type' => 'str',
 			'time' => 'str', // TS_MW
-			'info' => 'blob',
+			'info' => 'data', // handled specially by ChangeRow
 			'object_id' => 'str',
 			'user_id' => 'int',
 			'revision_id' => 'int',