0b12a5953fc68c367599d6525479ed7a8cc87e10
daniel <daniel.kinzler@wikimedia.de>
2013-03-12 21:39:35 +0100

    (bug 45566) in-process caching for entities.
    This provides in-process caching of entitites.
    Some refactoring and moving was necessary to allow this.
    
    In particular, the Store and StoreFactory infrastructure
    (and its equivalent on the client) was changed to support
    service objects that persist throughout the duration
    of a request.
    
    Change-Id: I8851c14ef2be361715483c23066e3303b011ffc0

---
+++ includes/store/CachingEntityLoader.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace Wikibase;
+use MWException;
+
+/**
+ * Implementation of EntityLookup that caches the obtained entities in memory.
+ * The cache is never invalidated or purged. There is no size limit.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.4
+ *
+ * @file
+ * @ingroup WikibaseLib
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ * @author Daniel Kinzler
+ *
+ * @todo: rename to CachingEntityLookup
+ */
+class CachingEntityLoader implements EntityLookup {
+
+	/**
+	 * @var Entity|null[]
+	 */
+	protected $loadedEntities = array();
+
+	/**
+	 * @var EntityLookup
+	 */
+	protected $lookup;
+
+	/**
+	 * @param EntityLookup $lookup The Lookup to use to load entities.
+	 */
+	public function __construct( EntityLookup $lookup ) {
+		$this->lookup = $lookup;
+	}
+
+	/**
+	 * Returns a cache key for the given item ID and optional revision
+	 *
+	 * @param EntityId $entityId
+	 * @param int|bool $revision
+	 *
+	 * @return string
+	 */
+	protected function getCacheKey( $entityId, $revision = false ) {
+		$key = $entityId->getPrefixedId();
+
+		if ( $revision ) {
+			$key .= ( '@' . $revision );
+		}
+
+		return $key;
+	}
+
+	/**
+	 * @see EntityLookup::getEntity
+	 *
+	 * @since 0.4
+	 *
+	 * @param EntityId $entityId
+	 * @param boolean|int $revision
+	 *
+	 * @return Entity|null
+	 */
+	public function getEntity( EntityId $entityId, $revision = false ) {
+		wfProfileIn( __METHOD__ );
+		$key = $this->getCacheKey( $entityId, $revision );
+
+		if ( !array_key_exists( $key, $this->loadedEntities ) ) {
+			$entity = $this->lookup->getEntity( $entityId, $revision );
+			$this->loadedEntities[$key] = $entity;
+		}
+
+		wfProfileOut( __METHOD__ );
+		return $this->loadedEntities[$key];
+	}
+
+	/**
+	 * @see EntityLookup::getEntities
+	 *
+	 * @since 0.4
+	 *
+	 * @param array $entityIds
+	 * @param array|bool $revision
+	 *
+	 * @return Entity|null[]
+	 */
+	public function getEntities( array $entityIds, $revision = false ) {
+		wfProfileIn( __METHOD__ );
+
+		$loaded = array();
+		$toLoad = array();
+
+		foreach ( $entityIds as $entityId ) {
+			if ( !( $entityId instanceof EntityId ) ) {
+				wfProfileOut( __METHOD__ );
+				throw new MWException( 'All entity ids passed to loadEntities must be an instance of EntityId' );
+			}
+
+			$prefixedId = $entityId->getPrefixedId();
+			$key = $this->getCacheKey( $entityId );
+
+			if ( array_key_exists( $key, $this->loadedEntities ) ) {
+				$loaded[$prefixedId] = $this->loadedEntities[$key];
+			}
+			else {
+				$toLoad[] = $entityId;
+			}
+		}
+
+		if ( $toLoad ) {
+			$newlyLoaded = $this->lookup->getEntities( $toLoad );
+			$loaded = array_merge( $loaded, $newlyLoaded );
+
+			$this->loadedEntities = array_merge( $this->loadedEntities, $newlyLoaded );
+		}
+
+		wfProfileOut( __METHOD__ );
+		return $loaded;
+	}
+
+	/**
+	 * Removes any entity with the given combination of ID and revision from the cache
+	 *
+	 * @since 0.4
+	 *
+	 * @param EntityId $entityId
+	 * @param int|bool $revision
+	 *
+	 * @return void
+	 */
+	public function purgeEntity( EntityId $entityId, $revision = false ) {
+		$key = $this->getCacheKey( $entityId, $revision );
+		unset( $this->loadedEntities[$key] );
+	}
+
+	/**
+	 * Adds the provided entities to the entity cache of the loader.
+	 * If an entity is already set, it will be overridden by the new value.
+	 *
+	 * @since 0.4
+	 *
+	 * @param Entity[] $entities
+	 *
+	 * @throws MWException
+	 */
+	public function setEntities( array $entities ) {
+		foreach ( $entities as $entity ) {
+			if ( !( $entity instanceof Entity ) ) {
+				throw new MWException( 'All entities passed to setEntities must be an instance of Entity' );
+			}
+
+			$key = $this->getCacheKey( $entity->getId() );
+			$this->loadedEntities[$key] = $entity;
+		}
+	}
+
+}
