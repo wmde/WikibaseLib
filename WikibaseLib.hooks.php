wikimedia/mediawiki-extensions-Wikibase

(Bug 47125) ChunkCache for speeding up dispatchChanges.
This allows changes to be cached between dispatcher passes, so the same change
does not have to be re-loaded for every target wiki.

Change-Id: I677d5fe46fcd7cf565443aa581f69e73c28fa940

lib/WikibaseLib.hooks.php
@@ -61,6 +61,7 @@ final class LibHooks {
 			'serializers/Serializer',
 			'serializers/SnakSerializer',
 
+			'store/ChunkCacheTest',
 			'store/SiteLinkLookup',
 			'store/SiteLinkTable',
 			'store/WikiPageEntityLookup',

wmde/WikibaseLib

WikibaseLib.hooks.php
@@ -61,6 +61,7 @@ final class LibHooks {
 			'serializers/Serializer',
 			'serializers/SnakSerializer',
 
+			'store/ChunkCache',
 			'store/SiteLinkLookup',
 			'store/SiteLinkTable',
 			'store/WikiPageEntityLookup',