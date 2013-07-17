wikimedia/mediawiki-extensions-Wikibase

(bug 50798) Allow dynamic site link group section headings in item view
Change-Id: Ibd63ad8b5f1d90628151412deacfe89e7dc94822

lib/resources/templates.php
@@ -46,6 +46,11 @@ HTML;
 <h2 class="wb-section-heading" dir="auto" id="$2">$1</h2>
 HTML;
 
+	$templates['wb-section-sitelink-heading'] =
+<<<HTML
+<h2 class="wb-section-heading $3" dir="auto" id="$2">$1</h2>
+HTML;
+
 	$templates['wb-claimlist'] =
 <<<HTML
 <div class="wb-claimlist">

wmde/WikibaseLib

resources/templates.php
@@ -46,6 +46,11 @@ HTML;
 <h2 class="wb-section-heading" dir="auto" id="$2">$1</h2>
 HTML;
 
+	$templates['wb-section-heading-sitelinks'] =
+<<<HTML
+<h2 class="wb-section-heading wb-sitelinks-heading" dir="auto" id="$2">$1</h2>
+HTML;
+
 	$templates['wb-claimlist'] =
 <<<HTML
 <div class="wb-claimlist">