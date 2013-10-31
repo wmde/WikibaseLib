wikimedia/mediawiki-extensions-Wikibase

Parse lowercase ids in ByPropertyListUnserializer
Change-Id: Iac8f4e7cbda907fde186532fea0fbfa4894a322b

lib/includes/serializers/ByPropertyListUnserializer.php
@@ -2,6 +2,9 @@
 
 namespace Wikibase\Lib\Serializers;
 use ApiResult, MWException;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Lib\EntityIdParser;
 
 /**
  * Serializer for Traversable objects that need to be grouped
@@ -62,6 +65,7 @@ class ByPropertyListUnserializer implements Unserializer {
 	 */
 	public function newFromSerialization( array $serialization ) {
 		$elements = array();
+		$idParser = new EntityIdParser();
 
 		foreach ( $serialization as $propertyId => $byPropId ) {
 			if ( !is_array( $byPropId ) ) {
@@ -70,11 +74,14 @@ class ByPropertyListUnserializer implements Unserializer {
 
 			foreach ( $byPropId as $serializedElement ) {
 				$element = $this->elementUnserializer->newFromSerialization( $serializedElement );
-				// FIXME: usage of deprecated method getPrefixedId
-				$elementPropertyId = $element->getPropertyId()->getPrefixedId();
 
-				if ( $elementPropertyId !== $propertyId ) {
-					throw new MWException( "Element with id '$elementPropertyId' found in list with id '$propertyId'" );
+				/** @var PropertyId $elementPropertyId */
+				$elementPropertyId = $element->getPropertyId();
+				$propertyId = $idParser->parse( $propertyId );
+
+				if ( !$elementPropertyId->equals( $propertyId ) ) {
+					throw new MWException( "Element with id '" . $elementPropertyId->getSerialization() .
+					"' found in list with id '" . $propertyId->getSerialization() . "'" );
 				}
 
 				$elements[] = $element;

wmde/WikibaseLib

includes/serializers/ByPropertyListUnserializer.php
@@ -2,6 +2,8 @@
 
 namespace Wikibase\Lib\Serializers;
 
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Lib\EntityIdParser;
 use InvalidArgumentException;
 use OutOfBoundsException;
 
@@ -47,18 +49,24 @@ class ByPropertyListUnserializer implements Unserializer {
 	 */
 	public function newFromSerialization( array $serialization ) {
 		$elements = array();
+		$idParser = new EntityIdParser();
 
 		foreach ( $serialization as $propertyId => $byPropId ) {
 			if ( !is_array( $byPropId ) ) {
 				throw new InvalidArgumentException( "Element with key '$propertyId' should be an array, found " . gettype( $byPropId ) );
 			}
 
+			$propertyId = $idParser->parse( $propertyId );
+
 			foreach ( $byPropId as $serializedElement ) {
 				$element = $this->elementUnserializer->newFromSerialization( $serializedElement );
-				$elementPropertyId = $element->getPropertyId()->getPrefixedId();
 
-				if ( $elementPropertyId !== $propertyId ) {
-					throw new OutOfBoundsException( "Element with id '$elementPropertyId' found in list with id '$propertyId'" );
+				/** @var PropertyId $elementPropertyId */
+				$elementPropertyId = $element->getPropertyId();
+
+				if ( !$elementPropertyId->equals( $propertyId ) ) {
+					throw new OutOfBoundsException( "Element with id '" . $elementPropertyId->getSerialization() .
+					"' found in list with id '" . $propertyId->getSerialization() . "'" );
 				}
 
 				$elements[] = $element;