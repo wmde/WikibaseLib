wikimedia/mediawiki-extensions-Wikibase

(bug 62491) Service for checking permissions.
This introduces the EntityPermissionChecker service and
removes any methods for permission checking from EntityContent.
That allows us to get rid of the last reason ApiWikibase
needed to depend on EntityContent.

Change-Id: I3323ba922e33171563ed69ffa64a64829b563ae1

lib/includes/store/EntityPermissionChecker.php
@@ -1,0 +1,69 @@
+<?php
+
+namespace Wikibase;
+
+use Status;
+use User;
+
+/**
+ * Service interface for checking a user's permissions on a given entity.
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+interface EntityPermissionChecker {
+
+	/**
+	 * Check whether the given user has the given permission on an entity.
+	 * This will perform a check based on the entity's ID if the entity has an ID set
+	 * (that is, the entity "exists"), or based merely on the entity type, in case
+	 * the entity does not exist.
+	 *
+	 * @param User $user
+	 * @param string $permission
+	 * @param Entity $entity
+	 * @param string $relaxed Flag for allowing relaxed permission checking. If set to
+	 * 'relaxed', implementations may return inaccurate results if determining the accurate result
+	 * is deemed expensive. This is intended as an optimization for non-critical checks,
+	 * e.g. for showing or hiding UI elements.
+	 *
+	 * @return Status a status object representing the check's result.
+	 */
+	public function getPermissionStatusForEntity( User $user, $permission, Entity $entity, $relaxed = '' );
+
+	/**
+	 * Check whether the given user has the given permission on an entity.
+	 * This requires the ID of an existing entity.
+	 *
+	 * @param User $user
+	 * @param string $permission
+	 * @param EntityId $entityId
+	 * @param string $relaxed Flag for allowing relaxed permission checking. If set to
+	 * 'relaxed', implementations may return inaccurate results if determining the accurate result
+	 * is deemed expensive. This is intended as an optimization for non-critical checks,
+	 * e.g. for showing or hiding UI elements.
+	 *
+	 * @return Status a status object representing the check's result.
+	 */
+	public function getPermissionStatusForEntityId( User $user, $permission, EntityId $entityId, $relaxed = '' );
+
+	/**
+	 * Check whether the given user has the given permission on a given entity type.
+	 * This does not require an entity to exist.
+	 *
+	 * Useful especially for checking whether the user is allowed to create an entity
+	 * of a given type.
+	 *
+	 * @param User $user
+	 * @param string $permission
+	 * @param string $type
+	 * @param string $relaxed Flag for allowing relaxed permission checking. If set to
+	 * 'relaxed', implementations may return inaccurate results if determining the accurate result
+	 * is deemed expensive. This is intended as an optimization for non-critical checks,
+	 * e.g. for showing or hiding UI elements.
+	 *
+	 * @return Status a status object representing the check's result.
+	 */
+	public function getPermissionStatusForEntityType( User $user, $permission, $type, $relaxed = '' );
+
+} 

wmde/WikibaseLib

includes/store/EntityPermissionChecker.php
@@ -1,0 +1,72 @@
+<?php
+
+namespace Wikibase;
+
+use Status;
+use User;
+
+/**
+ * Service interface for checking a user's permissions on a given entity.
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+interface EntityPermissionChecker {
+
+	/**
+	 * Check whether the given user has the given permission on an entity.
+	 * This will perform a check based on the entity's ID if the entity has an ID set
+	 * (that is, the entity "exists"), or based merely on the entity type, in case
+	 * the entity does not exist.
+	 *
+	 * @param User $user
+	 * @param string $permission
+	 * @param Entity $entity
+	 * @param string $quick Flag for allowing quick permission checking. If set to
+	 * 'quick', implementations may return inaccurate results if determining the accurate result
+	 * would be slow (e.g. checking for cascading protection).
+	 * This is intended as an optimization for non-critical checks,
+	 * e.g. for showing or hiding UI elements.
+	 *
+	 * @return Status a status object representing the check's result.
+	 */
+	public function getPermissionStatusForEntity( User $user, $permission, Entity $entity, $quick = '' );
+
+	/**
+	 * Check whether the given user has the given permission on an entity.
+	 * This requires the ID of an existing entity.
+	 *
+	 * @param User $user
+	 * @param string $permission
+	 * @param EntityId $entityId
+	 * @param string $quick Flag for allowing quick permission checking. If set to
+	 * 'quick', implementations may return inaccurate results if determining the accurate result
+	 * would be slow (e.g. checking for cascading protection).
+	 * This is intended as an optimization for non-critical checks,
+	 * e.g. for showing or hiding UI elements.
+	 *
+	 * @return Status a status object representing the check's result.
+	 */
+	public function getPermissionStatusForEntityId( User $user, $permission, EntityId $entityId, $quick = '' );
+
+	/**
+	 * Check whether the given user has the given permission on a given entity type.
+	 * This does not require an entity to exist.
+	 *
+	 * Useful especially for checking whether the user is allowed to create an entity
+	 * of a given type.
+	 *
+	 * @param User $user
+	 * @param string $permission
+	 * @param string $type
+	 * @param string $quick Flag for allowing quick permission checking. If set to
+	 * 'quick', implementations may return inaccurate results if determining the accurate result
+	 * would be slow (e.g. checking for cascading protection).
+	 * This is intended as an optimization for non-critical checks,
+	 * e.g. for showing or hiding UI elements.
+	 *
+	 * @return Status a status object representing the check's result.
+	 */
+	public function getPermissionStatusForEntityType( User $user, $permission, $type, $quick = '' );
+
+}