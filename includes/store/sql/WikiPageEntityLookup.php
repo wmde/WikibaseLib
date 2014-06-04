wikimedia/mediawiki-extensions-Wikibase

Introducing EntityContentDataCodec
This factors the basic serialization logic for EntityContent
into EntityContentDataCodec, removing any knowledge about the
serialization from EntityHandler and EntityFactory.

Change-Id: I90d38bda79aa10f2292432dda28dd97453d47321

lib/includes/store/sql/WikiPageEntityLookup.php
@@ -5,6 +5,7 @@ namespace Wikibase;
 use DBQueryError;
 use Wikibase\DataModel\Entity\BasicEntityIdParser;
 use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\Serializers\EntityContentDataCodec;
 
 /**
  * Implements an entity repo based on blobs stored in wiki pages on a locally reachable
@@ -24,11 +25,29 @@ class WikiPageEntityLookup extends \DBAccessBase implements EntityRevisionLookup
 	protected $idParser;
 
 	/**
-	 * @param string|bool $wiki The name of the wiki database to use (use false for the local wiki)
-	 *
+	 * @var EntityContentDataCodec
 	 */
-	public function __construct( $wiki = false ) {
+	private $contentCodec;
+
+	/**
+	 * @var EntityFactory
+	 */
+	private $entityFactory;
+
+	/**
+	 * @param EntityContentDataCodec $contentCodec
+	 * @param EntityFactory $entityFactory
+	 * @param string|bool $wiki The name of the wiki database to use (use false for the local wiki)
+	 */
+	public function __construct(
+		EntityContentDataCodec $contentCodec,
+		EntityFactory $entityFactory,
+		$wiki = false
+	) {
 		parent::__construct( $wiki );
+
+		$this->contentCodec = $contentCodec;
+		$this->entityFactory = $entityFactory;
 
 		// TODO: migrate table away from using a numeric field so we no longer need this!
 		$this->idParser = new BasicEntityIdParser();
@@ -335,9 +354,10 @@ class WikiPageEntityLookup extends \DBAccessBase implements EntityRevisionLookup
 	 * @param String $entityType The entity type ID, determines what kind of object is constructed
 	 *        from the blob in the database.
 	 *
-	 * @return EntityRevision|null
+	 * @return array list( EntityRevision|null $entityRev, EntityId|null $redirect ),
+	 * with either $entityRev or $redirect or both being null (but not both being non-null).
 	 */
-	protected function loadEntity( $entityType, $row ) {
+	private function loadEntity( $entityType, $row ) {
 		wfProfileIn( __METHOD__ );
 
 		wfDebugLog( __CLASS__, __FUNCTION__ . ": calling getRevisionText() on rev " . $row->rev_id );
@@ -355,7 +375,7 @@ class WikiPageEntityLookup extends \DBAccessBase implements EntityRevisionLookup
 		}
 
 		$format = $row->rev_content_format;
-		$entity = EntityFactory::singleton()->newFromBlob( $entityType, $blob, $format );
+		$entity = $this->unserializeEntity( $entityType, $blob, $format );
 		$entityRev = new EntityRevision( $entity, (int)$row->rev_id, $row->rev_timestamp );
 
 		wfDebugLog( __CLASS__, __FUNCTION__ . ": Created entity object from revision blob: "
@@ -364,4 +384,23 @@ class WikiPageEntityLookup extends \DBAccessBase implements EntityRevisionLookup
 		wfProfileOut( __METHOD__ );
 		return $entityRev;
 	}
+
+	/**
+	 * @see ContentHandler::unserializeContent
+	 *
+	 * @since 0.5
+	 *
+	 * @param $entityType
+	 * @param string $blob
+	 * @param null|string $format
+	 *
+	 * @return Entity|null
+	 */
+	private function unserializeEntity( $entityType, $blob, $format = null ) {
+		$data = $this->contentCodec->decodeBlob( $blob, $format );
+		$entity = $this->entityFactory->newFromArray( $entityType, $data );
+
+		return $entity;
+	}
+
 }

lib/tests/phpunit/serializers/EntityContentDataCodecTest.php
@@ -1,0 +1,60 @@
+<?php
+
+namespace Wikibase\Test;
+use Wikibase\Serializers\EntityContentDataCodec;
+
+/**
+ * @covers Wikibase\Serializers\EntityContentCodec
+ *
+ * @group Wikibase
+ * @group WikibaseLib
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class EntityContentDataCodecTest extends EntityTestCase {
+
+	protected function getCodec() {
+		return new EntityContentDataCodec();
+	}
+
+	public function encodeDecodeProvider() {
+		return array(
+			'empty' => array( array(), null ),
+			'empty json' => array( array(), CONTENT_FORMAT_JSON ),
+
+			'list' => array( array( 'a', 'b', 'c' ), null ),
+			'list json' => array( array( 'a', 'b', 'c' ), CONTENT_FORMAT_JSON ),
+		);
+	}
+
+	/**
+	 * @dataProvider encodeDecodeProvider
+	 */
+	public function testEncodeDecode( $data, $format ) {
+		$codec = $this->getCodec();
+
+		$blob = $codec->encodeBlob( $data, $format );
+		$this->assertType( 'string', $blob );
+
+		$actual = $codec->decodeBlob( $blob, $format );
+
+		$this->assertEquals( $data, $actual, 'round trip' );
+	}
+
+	public function testGetDefaultFormat() {
+		$codec = $this->getCodec();
+
+		$this->assertType( 'string', $codec->getDefaultFormat() );
+		$this->assertContains( $codec->getDefaultFormat(), $codec->getSupportedFormats() );
+	}
+
+	public function testGetSupportedFormats() {
+		$codec = $this->getCodec();
+
+		$supported = $codec->getSupportedFormats();
+		$this->assertType( 'array', $supported );
+		$this->assertContains( CONTENT_FORMAT_JSON, $codec->getSupportedFormats() );
+	}
+
+}

wmde/WikibaseLib

Bug: 66024

includes/store/sql/WikiPageEntityLookup.php
@@ -8,6 +8,7 @@ use Wikibase\DataModel\Entity\Entity;
 use Wikibase\DataModel\Entity\EntityId;
 use Wikibase\DataModel\Entity\EntityIdParser;
 use Wikibase\EntityFactory;
+use Wikibase\Lib\Store\EntityContentDataCodec;
 use Wikibase\EntityRevision;
 use Wikibase\StorageException;
 
@@ -29,11 +30,29 @@ class WikiPageEntityLookup extends \DBAccessBase implements EntityRevisionLookup
 	protected $idParser;
 
 	/**
-	 * @param string|bool $wiki The name of the wiki database to use (use false for the local wiki)
-	 *
+	 * @var EntityContentDataCodec
 	 */
-	public function __construct( $wiki = false ) {
+	private $contentCodec;
+
+	/**
+	 * @var EntityFactory
+	 */
+	private $entityFactory;
+
+	/**
+	 * @param EntityContentDataCodec $contentCodec
+	 * @param EntityFactory $entityFactory
+	 * @param string|bool $wiki The name of the wiki database to use (use false for the local wiki)
+	 */
+	public function __construct(
+		EntityContentDataCodec $contentCodec,
+		EntityFactory $entityFactory,
+		$wiki = false
+	) {
 		parent::__construct( $wiki );
+
+		$this->contentCodec = $contentCodec;
+		$this->entityFactory = $entityFactory;
 
 		// TODO: migrate table away from using a numeric field so we no longer need this!
 		$this->idParser = new BasicEntityIdParser();
@@ -340,9 +359,10 @@ class WikiPageEntityLookup extends \DBAccessBase implements EntityRevisionLookup
 	 * @param String $entityType The entity type ID, determines what kind of object is constructed
 	 *        from the blob in the database.
 	 *
-	 * @return EntityRevision|null
+	 * @return array list( EntityRevision|null $entityRev, EntityId|null $redirect ),
+	 * with either $entityRev or $redirect or both being null (but not both being non-null).
 	 */
-	protected function loadEntity( $entityType, $row ) {
+	private function loadEntity( $entityType, $row ) {
 		wfProfileIn( __METHOD__ );
 
 		wfDebugLog( __CLASS__, __FUNCTION__ . ": calling getRevisionText() on rev " . $row->rev_id );
@@ -360,7 +380,7 @@ class WikiPageEntityLookup extends \DBAccessBase implements EntityRevisionLookup
 		}
 
 		$format = $row->rev_content_format;
-		$entity = EntityFactory::singleton()->newFromBlob( $entityType, $blob, $format );
+		$entity = $this->unserializeEntity( $entityType, $blob, $format );
 		$entityRev = new EntityRevision( $entity, (int)$row->rev_id, $row->rev_timestamp );
 
 		wfDebugLog( __CLASS__, __FUNCTION__ . ": Created entity object from revision blob: "
@@ -369,4 +389,23 @@ class WikiPageEntityLookup extends \DBAccessBase implements EntityRevisionLookup
 		wfProfileOut( __METHOD__ );
 		return $entityRev;
 	}
+
+	/**
+	 * @see ContentHandler::unserializeContent
+	 *
+	 * @since 0.5
+	 *
+	 * @param $entityType
+	 * @param string $blob
+	 * @param null|string $format
+	 *
+	 * @return Entity|null
+	 */
+	private function unserializeEntity( $entityType, $blob, $format = null ) {
+		$data = $this->contentCodec->decodeEntityContentData( $blob, $format );
+		$entity = $this->entityFactory->newFromArray( $entityType, $data );
+
+		return $entity;
+	}
+
 }

tests/phpunit/store/EntityContentDataCodecTest.php
@@ -1,0 +1,62 @@
+<?php
+
+namespace Wikibase\Store\Test;
+
+use Wikibase\Lib\Store\EntityContentDataCodec;
+use Wikibase\Test\EntityTestCase;
+
+/**
+ * @covers Wikibase\Serializers\EntityContentCodec
+ *
+ * @group Wikibase
+ * @group WikibaseLib
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class EntityContentDataCodecTest extends EntityTestCase {
+
+	protected function getCodec() {
+		return new EntityContentDataCodec();
+	}
+
+	public function encodeDecodeProvider() {
+		return array(
+			'empty' => array( array(), null ),
+			'empty json' => array( array(), CONTENT_FORMAT_JSON ),
+
+			'list' => array( array( 'a', 'b', 'c' ), null ),
+			'list json' => array( array( 'a', 'b', 'c' ), CONTENT_FORMAT_JSON ),
+		);
+	}
+
+	/**
+	 * @dataProvider encodeDecodeProvider
+	 */
+	public function testEncodeEntityContentData( $data, $format ) {
+		$codec = $this->getCodec();
+
+		$blob = $codec->encodeEntityContentData( $data, $format );
+		$this->assertType( 'string', $blob );
+
+		$actual = $codec->decodeEntityContentData( $blob, $format );
+
+		$this->assertEquals( $data, $actual, 'round trip' );
+	}
+
+	public function testGetDefaultFormat() {
+		$codec = $this->getCodec();
+
+		$this->assertType( 'string', $codec->getDefaultFormat() );
+		$this->assertContains( $codec->getDefaultFormat(), $codec->getSupportedFormats() );
+	}
+
+	public function testGetSupportedFormats() {
+		$codec = $this->getCodec();
+
+		$supported = $codec->getSupportedFormats();
+		$this->assertType( 'array', $supported );
+		$this->assertContains( CONTENT_FORMAT_JSON, $codec->getSupportedFormats() );
+	}
+
+}