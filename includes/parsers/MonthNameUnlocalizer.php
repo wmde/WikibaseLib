wikimedia/mediawiki-extensions-Wikibase

Drop unused parameter from MonthNameUnlocalizer
Change-Id: Ia322f83dec6eaffcadb25fbacaf94944234e2a48

lib/includes/parsers/MonthNameUnlocalizer.php
@@ -3,7 +3,6 @@
 namespace Wikibase\Lib\Parsers;
 
 use Language;
-use ValueParsers\ParserOptions;
 
 /**
  * Class to unlocalise month names using Mediawiki's Language object
@@ -22,11 +21,10 @@ class MonthNameUnlocalizer {
 	 *
 	 * @param string $string string to process
 	 * @param string $languageCode
-	 * @param ParserOptions $options
 	 *
 	 * @return string unlocalized string
 	 */
-	public function unlocalize( $string, $languageCode, ParserOptions $options ) {
+	public function unlocalize( $string, $languageCode ) {
 		if ( $languageCode === 'en' ) {
 			return $string;
 		}

wmde/WikibaseLib

includes/parsers/MonthNameUnlocalizer.php
@@ -3,7 +3,6 @@
 namespace Wikibase\Lib\Parsers;
 
 use Language;
-use ValueParsers\ParserOptions;
 
 /**
  * Class to unlocalize a month name in a date string using MediaWiki's Language object.
@@ -22,11 +21,10 @@ class MonthNameUnlocalizer {
 	 *
 	 * @param string $date Localized date string.
 	 * @param string $languageCode
-	 * @param ParserOptions $options
 	 *
 	 * @return string
 	 */
-	public function unlocalize( $date, $languageCode, ParserOptions $options ) {
+	public function unlocalize( $date, $languageCode ) {
 		if ( $languageCode === 'en' ) {
 			return $date;
 		}