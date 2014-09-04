wikimedia/mediawiki-extensions-Wikibase

Integrated fingerprintgroupview into entityview
Change-Id: I38cc93a9dbfae2ca9b2b04d2e12de58a0850acf0

lib/resources/jquery.wikibase/jquery.wikibase.entityview.js
@@ -17,6 +17,7 @@
  * @option {wikibase.store.EntityStore} entityStore
  * @option {wikibase.ValueViewBuilder} valueViewBuilder
  * @option {wikibase.Api} api
+ * @option {string[]} languages
  */
 $.widget( 'wikibase.entityview', PARENT, {
 	/**
@@ -35,8 +36,14 @@ $.widget( 'wikibase.entityview', PARENT, {
 		value: null,
 		entityStore: null,
 		valueViewBuilder: null,
-		api: null
+		api: null,
+		languages: []
 	},
+
+	/**
+	 * @type {jQuery}
+	 */
+	$toc: null,
 
 	/**
 	 * @type {jQuery}
@@ -52,6 +59,11 @@ $.widget( 'wikibase.entityview', PARENT, {
 	 * @type {jQuery}
 	 */
 	$aliases: null,
+
+	/**
+	 * @type {jQuery}
+	 */
+	$fingerprints: null,
 
 	/**
 	 * @type {jQuery}
@@ -72,9 +84,12 @@ $.widget( 'wikibase.entityview', PARENT, {
 			throw new Error( 'Required option(s) missing' );
 		}
 
+		this.$toc = $( '.toc', this.element );
+
 		this._initLabel();
 		this._initDescription();
 		this._initAliases();
+		this._initFingerprints();
 		this._initClaims();
 		this._initSiteLinks();
 
@@ -142,6 +157,45 @@ $.widget( 'wikibase.entityview', PARENT, {
 				language:  mw.config.get( 'wgUserLanguage' ),
 				aliases: this.options.value.getAliases( mw.config.get( 'wgUserLanguage' ) )
 			},
+			entityId: this.options.value.getId(),
+			api: this.options.api
+		} );
+	},
+
+	_initFingerprints: function() {
+		if( !this.options.languages.length ) {
+			return;
+		}
+
+		this.$fingerprints = $( '.wikibase-fingerprintgroupview' );
+
+		if( !this.$fingerprints.length ) {
+			var $precedingNode = this.$toc;
+
+			if( !$precedingNode.length ) {
+				$precedingNode = $( '.wikibase-aliasesview' );
+			} else {
+				this._addTocItem(
+					'#wb-terms',
+					mw.msg( 'wikibase-terms' ),
+					this.$toc.find( 'li' ).first()
+				);
+			}
+
+			this.$fingerprints = $( '<div/>' ).appendTo( $precedingNode );
+		}
+
+		var value = [];
+		for( var i = 0; i < this.options.languages.length; i++ ) {
+			value.push( {
+				language: this.options.languages[i],
+				label: this.options.value.getLabel( this.options.languages[i] ) || null,
+				description: this.options.value.getDescription( this.options.languages[i] ) || null
+			} );
+		}
+
+		this.$fingerprints.fingerprintgroupview( {
+			value: value,
 			entityId: this.options.value.getId(),
 			api: this.options.api
 		} );
@@ -380,6 +434,35 @@ $.widget( 'wikibase.entityview', PARENT, {
 				] );
 			}
 		);
+	},
+
+	/**
+	 * Adds an item to the table of contents.
+	 *
+	 * @param {string} href
+	 * @param {string} text
+	 * @param {jQuery} [$insertBefore] Omit to have the item inserted at the end
+	 */
+	_addTocItem: function( href, text, $insertBefore ) {
+		if( !this.$toc.length ) {
+			return;
+		}
+
+		var $li = $( '<li>' )
+			.addClass( 'toclevel-1' )
+			.append( $( '<a>' ).attr( 'href', href ).text( text ) );
+
+		if( $insertBefore ) {
+			$li.insertBefore( $insertBefore );
+		} else {
+			this.$toc.append( $li );
+		}
+
+		this.$toc.find( 'li' ).each( function( i, li ) {
+			$( li )
+			.removeClass( 'tocsection-' + i )
+			.addClass( 'tocsection-' + ( i + 1 ) );
+		} );
 	}
 } );
 

wmde/WikibaseLib

resources/jquery.wikibase/jquery.wikibase.entityview.js
@@ -17,6 +17,7 @@
  * @option {wikibase.store.EntityStore} entityStore
  * @option {wikibase.ValueViewBuilder} valueViewBuilder
  * @option {wikibase.AbstractedRepoApi} api
+ * @option {string[]} languages
  */
 $.widget( 'wikibase.entityview', PARENT, {
 	/**
@@ -35,8 +36,14 @@ $.widget( 'wikibase.entityview', PARENT, {
 		value: null,
 		entityStore: null,
 		valueViewBuilder: null,
-		api: null
+		api: null,
+		languages: []
 	},
+
+	/**
+	 * @type {jQuery}
+	 */
+	$toc: null,
 
 	/**
 	 * @type {jQuery}
@@ -52,6 +59,11 @@ $.widget( 'wikibase.entityview', PARENT, {
 	 * @type {jQuery}
 	 */
 	$aliases: null,
+
+	/**
+	 * @type {jQuery}
+	 */
+	$fingerprints: null,
 
 	/**
 	 * @type {jQuery}
@@ -72,9 +84,12 @@ $.widget( 'wikibase.entityview', PARENT, {
 			throw new Error( 'Required option(s) missing' );
 		}
 
+		this.$toc = $( '.toc', this.element );
+
 		this._initLabel();
 		this._initDescription();
 		this._initAliases();
+		this._initFingerprints();
 		this._initClaims();
 		this._initSiteLinks();
 
@@ -93,7 +108,7 @@ $.widget( 'wikibase.entityview', PARENT, {
 		this.$label.labelview( {
 			value: {
 				language: mw.config.get( 'wgUserLanguage' ),
-				label: $( '.wikibase-labelview' ).hasClass( 'wb-empty' )
+				label: this.$label.hasClass( 'wb-empty' )
 					? null
 					// FIXME: entity object should not contain fallback strings
 					: this.options.value.getLabel( mw.config.get( 'wgUserLanguage' ) )
@@ -117,7 +132,7 @@ $.widget( 'wikibase.entityview', PARENT, {
 		this.$description.descriptionview( {
 			value: {
 				language: mw.config.get( 'wgUserLanguage' ),
-				description: $( '.wikibase-descriptionview', this.element ).hasClass( 'wb-empty' )
+				description: this.$description.hasClass( 'wb-empty' )
 					? null
 					// FIXME: entity object should not contain fallback strings
 					: this.options.value.getDescription( mw.config.get( 'wgUserLanguage' ) )
@@ -142,6 +157,45 @@ $.widget( 'wikibase.entityview', PARENT, {
 				language:  mw.config.get( 'wgUserLanguage' ),
 				aliases: this.options.value.getAliases( mw.config.get( 'wgUserLanguage' ) )
 			},
+			entityId: this.options.value.getId(),
+			api: this.options.api
+		} );
+	},
+
+	_initFingerprints: function() {
+		if( !this.options.languages.length ) {
+			return;
+		}
+
+		this.$fingerprints = $( '.wikibase-fingerprintgroupview' );
+
+		if( !this.$fingerprints.length ) {
+			var $precedingNode = this.$toc;
+
+			if( !$precedingNode.length ) {
+				$precedingNode = $( '.wikibase-aliasesview' );
+			} else {
+				this._addTocItem(
+					'#wb-terms',
+					mw.msg( 'wikibase-terms' ),
+					this.$toc.find( 'li' ).first()
+				);
+			}
+
+			this.$fingerprints = $( '<div/>' ).insertAfter( $precedingNode );
+		}
+
+		var value = [];
+		for( var i = 0; i < this.options.languages.length; i++ ) {
+			value.push( {
+				language: this.options.languages[i],
+				label: this.options.value.getLabel( this.options.languages[i] ) || null,
+				description: this.options.value.getDescription( this.options.languages[i] ) || null
+			} );
+		}
+
+		this.$fingerprints.fingerprintgroupview( {
+			value: value,
 			entityId: this.options.value.getId(),
 			api: this.options.api
 		} );
@@ -380,6 +434,35 @@ $.widget( 'wikibase.entityview', PARENT, {
 				] );
 			}
 		);
+	},
+
+	/**
+	 * Adds an item to the table of contents.
+	 *
+	 * @param {string} href
+	 * @param {string} text
+	 * @param {jQuery} [$insertBefore] Omit to have the item inserted at the end
+	 */
+	_addTocItem: function( href, text, $insertBefore ) {
+		if( !this.$toc.length ) {
+			return;
+		}
+
+		var $li = $( '<li>' )
+			.addClass( 'toclevel-1' )
+			.append( $( '<a>' ).attr( 'href', href ).text( text ) );
+
+		if( $insertBefore ) {
+			$li.insertBefore( $insertBefore );
+		} else {
+			this.$toc.append( $li );
+		}
+
+		this.$toc.find( 'li' ).each( function( i, li ) {
+			$( li )
+			.removeClass( 'tocsection-' + i )
+			.addClass( 'tocsection-' + ( i + 1 ) );
+		} );
 	}
 } );
 