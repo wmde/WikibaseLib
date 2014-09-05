wikimedia/mediawiki-extensions-Wikibase

Added aliasesview to fingerprintview
Change-Id: I12309c6bdebbe7461c7922f1e8c78e8c5aeeab31

lib/resources/jquery.wikibase/jquery.wikibase.fingerprintview.js
@@ -50,27 +50,30 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 				return this.options.value.language;
 			},
 			function() {
-				return wb.getLanguageNameByCode( this.options.value.language );
-			},
-			'', // label
-			'', // description
-			'', // label toolbar
-			'', // description toolbar
-			'', // additional label CSS classes
-			'', // additional description CSS classes
-			function() {
 				var title = new mw.Title(
 					mw.config.get( 'wgTitle' ),
 					mw.config.get( 'wgNamespaceNumber' )
 				);
-
 				return title.getUrl( { setlang: this.options.value.language } );
-			}
+			},
+			function() {
+				return wb.getLanguageNameByCode( this.options.value.language );
+			},
+			'', // additional label CSS classes
+			'', // label
+			'', // label toolbar
+			'', // additional description CSS classes
+			'', // description
+			'', // description toolbar
+			'', // additional aliases CSS classes
+			'', // aliases
+			'' // aliases toolbar
 		],
 		templateShortCuts: {
 			$language: '.wikibase-fingerprintview-language',
 			$label: '.wikibase-fingerprintview-label',
-			$description : '.wikibase-fingerprintview-description'
+			$description : '.wikibase-fingerprintview-description',
+			$aliases : '.wikibase-fingerprintview-aliases'
 		},
 		value: null,
 		helpMessage: mw.msg( 'wikibase-fingerprintview-input-help-message' ),
@@ -107,6 +110,9 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 			if( self.$description ) {
 				self.$description.data( 'descriptionview' ).destroy();
 			}
+			if( self.$aliases ) {
+				self.$aliases.data( 'aliasesview' ).destroy();
+			}
 
 			PARENT.prototype.destroy.call( self );
 		}
@@ -128,11 +134,11 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 	_createWidgets: function() {
 		var self = this;
 
-		$.each( ['label', 'description'], function( i, subjectName ) {
+		$.each( ['label', 'description', 'aliases'], function( i, subjectName ) {
 			var widgetName = subjectName + 'view',
 				$subject = self['$' + subjectName];
 
-			// Fully encapsulate labelview and descriptionview by suppressing their events:
+			// Fully encapsulate child widgets by suppressing their events:
 			$subject
 			.on( widgetName + 'change', function( event ) {
 				event.stopPropagation();
@@ -174,7 +180,8 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 	 */
 	isValid: function() {
 		return this.$label.data( 'labelview' ).isValid()
-			&& this.$description.data( 'descriptionview' ).isValid();
+			&& this.$description.data( 'descriptionview' ).isValid()
+			&& this.$aliases.data( 'aliasesview' ).isValid();
 	},
 
 	/**
@@ -182,7 +189,8 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 	 */
 	isInitialValue: function() {
 		return this.$label.data( 'labelview' ).isInitialValue()
-			&& this.$description.data( 'descriptionview' ).isInitialValue();
+			&& this.$description.data( 'descriptionview' ).isInitialValue()
+			&& this.$aliases.data( 'aliasesview' ).isInitialValue();
 	},
 
 	/**
@@ -198,6 +206,7 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 
 		this.$label.data( 'labelview' ).startEditing();
 		this.$description.data( 'descriptionview' ).startEditing();
+		this.$aliases.data( 'aliasesview' ).startEditing();
 
 		this._trigger( 'afterstartediting' );
 	},
@@ -222,21 +231,22 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 
 		var labelview = this.$label.data( 'labelview' ),
 			descriptionview = this.$description.data( 'descriptionview' ),
-			stoppedEditing = false;
+			aliasesview = this.$aliases.data( 'aliasesview' ),
+			stoppedEditing = 0;
 
 		function detectAfterStopEditing() {
-			if( !stoppedEditing ) {
-				stoppedEditing = true;
-				return;
+			if( stoppedEditing++ === 2 ) {
+				self._afterStopEditing( dropValue );
 			}
-			self._afterStopEditing( dropValue );
 		}
 
 		this.$label.one( 'labelviewafterstopediting', detectAfterStopEditing );
 		this.$description.one( 'descriptionviewafterstopediting', detectAfterStopEditing );
+		this.$aliases.one( 'aliasesviewafterstopediting', detectAfterStopEditing );
 
 		labelview.stopEditing( dropValue || labelview.isInitialValue() );
 		descriptionview.stopEditing( dropValue || descriptionview.isInitialValue() );
+		aliasesview.stopEditing( dropValue || aliasesview.isInitialValue() );
 	},
 
 	/**
@@ -270,7 +280,8 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 		return {
 			language: this.options.value.language,
 			label: this.$label.data( 'labelview' ).value().label,
-			description: this.$description.data( 'descriptionview' ).value().description
+			description: this.$description.data( 'descriptionview' ).value().description,
+			aliases: this.$aliases.data( 'aliasesview' ).value().aliases
 		};
 	},
 
@@ -296,6 +307,11 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 				language: value.language,
 				description: value.description
 			} );
+
+			this.$aliases.data( 'aliasesview' ).option( 'value', {
+				language: value.language,
+				aliases: value.aliases
+			} );
 		}
 		return PARENT.prototype._setOption.call( this, key, value );
 	},
@@ -319,6 +335,10 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 
 		if( !value.description ) {
 			value.description = null;
+		}
+
+		if( !value.aliases ) {
+			value.aliases = [];
 		}
 
 		return value;
@@ -358,7 +378,7 @@ $.wikibase.toolbarcontroller.definition( 'edittoolbar', {
 				fingerprintview = $fingerprintview.data( 'fingerprintview' );
 
 			$fingerprintview.edittoolbar( {
-				$container: $( '<td rowspan="2" />' )
+				$container: $( '<td rowspan="3" />' )
 					.appendTo( $fingerprintview.children( 'tr' ).first() ),
 				interactionWidgetName: $.wikibase.fingerprintview.prototype.widgetName,
 				enableRemove: false
@@ -384,7 +404,7 @@ $.wikibase.toolbarcontroller.definition( 'edittoolbar', {
 		},
 		toolbareditgroupedit: function( event, toolbarcontroller ) {
 			var $fingerprintview = $( event.target ).closest( ':wikibase-edittoolbar' ),
-				fingerprintview = $fingerprintview.data( 'labelview' );
+				fingerprintview = $fingerprintview.data( 'fingerprintview' );
 
 			if( !fingerprintview ) {
 				return;

lib/tests/qunit/jquery.wikibase/jquery.wikibase.fingerprintview.tests.js
@@ -18,18 +18,22 @@ var createFingerprintview = function( options, $node ) {
 		value: {
 			language: 'en',
 			label: 'test label',
-			description: 'test description'
+			description: 'test description',
+			aliases: ['alias1', 'alias2']
 		}
 	}, options || {} );
 
-	$node = $node || $( '<div/>' ).appendTo( 'body' );
+	$node = $node || $( '<tbody/>' ).appendTo( $( '<table/>' ) );
 
 	var $fingerprintview = $node
 		.addClass( 'test_fingerprintview' )
 		.fingerprintview( options );
 
-	$fingerprintview.data( 'fingerprintview' ).$label.data( 'labelview' )._save
-		= $fingerprintview.data( 'fingerprintview' ).$description.data( 'descriptionview' )._save
+	var fingerprintview = $fingerprintview.data( 'fingerprintview' );
+
+	fingerprintview.$label.data( 'labelview' )._save
+		= fingerprintview.$description.data( 'descriptionview' )._save
+		= fingerprintview.$aliases.data( 'aliasesview' )._save
 		= function() {
 			return $.Deferred().resolve( {
 				entity: {
@@ -171,7 +175,8 @@ QUnit.test( 'value()', function( assert ) {
 	fingerprintview.value( {
 		language: 'en',
 		label: 'changed label',
-		description: 'test description'
+		description: 'test description',
+		aliases: ['alias1', 'alias2']
 	} );
 
 	assert.ok(
@@ -195,7 +200,8 @@ QUnit.test( 'value()', function( assert ) {
 	fingerprintview.value( {
 		language: 'en',
 		label: 'test label',
-		description: null
+		description: null,
+		aliases: ['alias1', 'alias2']
 	} );
 
 	assert.equal(
@@ -210,12 +216,39 @@ QUnit.test( 'value()', function( assert ) {
 		'Removed description.'
 	);
 
+	fingerprintview.value( {
+		language: 'en',
+		label: 'test label',
+		description: null,
+		aliases: ['alias1', 'alias2', 'alias3']
+	} );
+
+	assert.deepEqual(
+		fingerprintview.value().aliases,
+		['alias1', 'alias2', 'alias3'],
+		'Added alias.'
+	);
+
+	fingerprintview.value( {
+		language: 'en',
+		label: 'test label',
+		description: null,
+		aliases: []
+	} );
+
+	assert.deepEqual(
+		fingerprintview.value().aliases,
+		[],
+		'Removed aliases.'
+	);
+
 	assert.throws(
 		function() {
 			fingerprintview.value( {
 				language: 'de',
 				label: 'test label',
-				description: null
+				description: null,
+				aliases: ['alias1', 'alias2']
 			} );
 		},
 		'Trying to change language fails.'

wmde/WikibaseLib

resources/jquery.wikibase/jquery.wikibase.fingerprintview.js
@@ -14,7 +14,12 @@
  *
  * @option {Object|null} value
  *         Object representing the widget's value.
- *         Structure: { language: <{string}>, label: <{string|null}>, description: <{string|null> }
+ *         Structure: {
+ *           language: <{string}>,
+ *           label: <{string|null}>,
+ *           description: <{string|null>,
+ *           aliases: <{string[]}>
+ *         }
  *
  * @option {string} [helpMessage]
  *         Default: mw.msg( 'wikibase-fingerprintview-input-help-message' )
@@ -50,33 +55,48 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 				return this.options.value.language;
 			},
 			function() {
-				return wb.getLanguageNameByCode( this.options.value.language );
-			},
-			'', // label
-			'', // description
-			'', // label toolbar
-			'', // description toolbar
-			'', // additional label CSS classes
-			'', // additional description CSS classes
-			function() {
 				var title = new mw.Title(
 					mw.config.get( 'wgTitle' ),
 					mw.config.get( 'wgNamespaceNumber' )
 				);
-
 				return title.getUrl( { setlang: this.options.value.language } );
-			}
+			},
+			function() {
+				return wb.getLanguageNameByCode( this.options.value.language );
+			},
+			'', // label
+			'', // label toolbar
+			'', // description
+			'', // description toolbar
+			'', // aliases
+			'' // aliases toolbar
 		],
 		templateShortCuts: {
 			$language: '.wikibase-fingerprintview-language',
 			$label: '.wikibase-fingerprintview-label',
-			$description : '.wikibase-fingerprintview-description'
+			$description : '.wikibase-fingerprintview-description',
+			$aliases : '.wikibase-fingerprintview-aliases'
 		},
 		value: null,
 		helpMessage: mw.msg( 'wikibase-fingerprintview-input-help-message' ),
 		entityId: null,
 		api: null
 	},
+
+	/**
+	 * @type {jQuery}
+	 */
+	$labelview: null,
+
+	/**
+	 * @type {jQuery}
+	 */
+	$descriptionview: null,
+
+	/**
+	 * @type {jQuery}
+	 */
+	$aliasesview: null,
 
 	/**
 	 * @type {boolean}
@@ -92,6 +112,25 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 		PARENT.prototype._create.call( this );
 
 		this._createWidgets();
+
+		var self = this;
+		this.element
+		// TODO: Move that code to a sensible place (see jQuery.wikibase.entityview):
+		.on( 'fingerprintviewafterstartediting.' + this.widgetName, function( event ) {
+			$( wb ).trigger( 'startItemPageEditMode', [
+				self.element,
+				{
+					exclusive: false,
+					wbCopyrightWarningGravity: 'sw'
+				}
+			] );
+		} )
+		.on( 'fingerprintviewafterstopediting.' + this.widgetName, function( event, dropValue ) {
+			$( wb ).trigger( 'stopItemPageEditMode', [
+				self.element,
+				{ save: dropValue !== true }
+			] );
+		} );
 	},
 
 	/**
@@ -101,11 +140,14 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 		var self = this;
 
 		function degrade() {
-			if( self.$label ) {
-				self.$label.data( 'labelview' ).destroy();
+			if( self.$labelview ) {
+				self.$labelview.data( 'labelview' ).destroy();
 			}
-			if( self.$description ) {
-				self.$description.data( 'descriptionview' ).destroy();
+			if( self.$descriptionview ) {
+				self.$descriptionview.data( 'descriptionview' ).destroy();
+			}
+			if( self.$aliasesview ) {
+				self.$aliasesview.data( 'aliasesview' ).destroy();
 			}
 
 			PARENT.prototype.destroy.call( self );
@@ -128,12 +170,17 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 	_createWidgets: function() {
 		var self = this;
 
-		$.each( ['label', 'description'], function( i, subjectName ) {
-			var widgetName = subjectName + 'view',
-				$subject = self['$' + subjectName];
+		$.each( ['label', 'description', 'aliases'], function( i, subjectName ) {
+			var widgetName = subjectName + 'view';
 
-			// Fully encapsulate labelview and descriptionview by suppressing their events:
-			$subject
+			self['$' + widgetName] = self['$' + subjectName].children( '.wikibase-' + widgetName );
+
+			if( !self['$' + widgetName].length ) {
+				self['$' + widgetName] = $( '<div/>' ).appendTo( self['$' + subjectName] );
+			}
+
+			// Fully encapsulate child widgets by suppressing their events:
+			self['$' + widgetName]
 			.on( widgetName + 'change', function( event ) {
 				event.stopPropagation();
 				self._trigger( 'change' );
@@ -152,7 +199,7 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 				}
 			);
 
-			$subject[widgetName]( {
+			self['$' + widgetName][widgetName]( {
 				value: self.options.value,
 				helpMessage: mw.msg(
 					'wikibase-' + subjectName + '-input-help-message',
@@ -161,6 +208,11 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 				entityId: self.options.entityId,
 				api: self.options.api
 			} );
+
+			// Remove events that would trigger startItemPageEditMode/stopItemPageEditMode:
+			// TODO: Once this widget is managed by entityview, handling those events should be done
+			// there.
+			self['$' + widgetName].off( '.' + widgetName );
 		} );
 	},
 
@@ -168,16 +220,18 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 	 * @return {boolean}
 	 */
 	isValid: function() {
-		return this.$label.data( 'labelview' ).isValid()
-			&& this.$description.data( 'descriptionview' ).isValid();
+		return this.$labelview.data( 'labelview' ).isValid()
+			&& this.$descriptionview.data( 'descriptionview' ).isValid()
+			&& this.$aliasesview.data( 'aliasesview' ).isValid();
 	},
 
 	/**
 	 * @return {boolean}
 	 */
 	isInitialValue: function() {
-		return this.$label.data( 'labelview' ).isInitialValue()
-			&& this.$description.data( 'descriptionview' ).isInitialValue();
+		return this.$labelview.data( 'labelview' ).isInitialValue()
+			&& this.$descriptionview.data( 'descriptionview' ).isInitialValue()
+			&& this.$aliasesview.data( 'aliasesview' ).isInitialValue();
 	},
 
 	/**
@@ -191,8 +245,9 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 		this._isInEditMode = true;
 		this.element.addClass( 'wb-edit' );
 
-		this.$label.data( 'labelview' ).startEditing();
-		this.$description.data( 'descriptionview' ).startEditing();
+		this.$labelview.data( 'labelview' ).startEditing();
+		this.$descriptionview.data( 'descriptionview' ).startEditing();
+		this.$aliasesview.data( 'aliasesview' ).startEditing();
 
 		this._trigger( 'afterstartediting' );
 	},
@@ -215,23 +270,24 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 
 		this.disable();
 
-		var labelview = this.$label.data( 'labelview' ),
-			descriptionview = this.$description.data( 'descriptionview' ),
-			stoppedEditing = false;
+		var labelview = this.$labelview.data( 'labelview' ),
+			descriptionview = this.$descriptionview.data( 'descriptionview' ),
+			aliasesview = this.$aliasesview.data( 'aliasesview' ),
+			stoppedEditing = 0;
 
 		function detectAfterStopEditing() {
-			if( !stoppedEditing ) {
-				stoppedEditing = true;
-				return;
+			if( stoppedEditing++ === 2 ) {
+				self._afterStopEditing( dropValue );
 			}
-			self._afterStopEditing( dropValue );
 		}
 
-		this.$label.one( 'labelviewafterstopediting', detectAfterStopEditing );
-		this.$description.one( 'descriptionviewafterstopediting', detectAfterStopEditing );
+		this.$labelview.one( 'labelviewafterstopediting', detectAfterStopEditing );
+		this.$descriptionview.one( 'descriptionviewafterstopediting', detectAfterStopEditing );
+		this.$aliasesview.one( 'aliasesviewafterstopediting', detectAfterStopEditing );
 
 		labelview.stopEditing( dropValue || labelview.isInitialValue() );
 		descriptionview.stopEditing( dropValue || descriptionview.isInitialValue() );
+		aliasesview.stopEditing( dropValue || aliasesview.isInitialValue() );
 	},
 
 	/**
@@ -264,8 +320,9 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 
 		return {
 			language: this.options.value.language,
-			label: this.$label.data( 'labelview' ).value().label,
-			description: this.$description.data( 'descriptionview' ).value().description
+			label: this.$labelview.data( 'labelview' ).value().label,
+			description: this.$descriptionview.data( 'descriptionview' ).value().description,
+			aliases: this.$aliasesview.data( 'aliasesview' ).value().aliases
 		};
 	},
 
@@ -282,14 +339,19 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 				throw new Error( 'Cannot alter language' );
 			}
 
-			this.$label.data( 'labelview' ).option( 'value', {
+			this.$labelview.data( 'labelview' ).option( 'value', {
 				language: value.language,
 				label: value.label
 			} );
 
-			this.$description.data( 'descriptionview' ).option( 'value', {
+			this.$descriptionview.data( 'descriptionview' ).option( 'value', {
 				language: value.language,
 				description: value.description
+			} );
+
+			this.$aliasesview.data( 'aliasesview' ).option( 'value', {
+				language: value.language,
+				aliases: value.aliases
 			} );
 		}
 		return PARENT.prototype._setOption.call( this, key, value );
@@ -316,6 +378,10 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 			value.description = null;
 		}
 
+		if( !value.aliases ) {
+			value.aliases = [];
+		}
+
 		return value;
 	},
 
@@ -323,7 +389,7 @@ $.widget( 'wikibase.fingerprintview', PARENT, {
 	 * Sets keyboard focus on the first input element.
 	 */
 	focus: function() {
-		this.$label.data( 'labelview' ).focus();
+		this.$labelview.data( 'labelview' ).focus();
 	},
 
 	/**
@@ -353,7 +419,7 @@ $.wikibase.toolbarcontroller.definition( 'edittoolbar', {
 				fingerprintview = $fingerprintview.data( 'fingerprintview' );
 
 			$fingerprintview.edittoolbar( {
-				$container: $( '<td rowspan="2" />' )
+				$container: $( '<td rowspan="3" />' )
 					.appendTo( $fingerprintview.children( 'tr' ).first() ),
 				interactionWidgetName: $.wikibase.fingerprintview.prototype.widgetName,
 				enableRemove: false
@@ -382,7 +448,7 @@ $.wikibase.toolbarcontroller.definition( 'edittoolbar', {
 		},
 		toolbareditgroupedit: function( event, toolbarcontroller ) {
 			var $fingerprintview = $( event.target ).closest( ':wikibase-edittoolbar' ),
-				fingerprintview = $fingerprintview.data( 'labelview' );
+				fingerprintview = $fingerprintview.data( 'fingerprintview' );
 
 			if( !fingerprintview ) {
 				return;

tests/qunit/jquery.wikibase/jquery.wikibase.fingerprintview.tests.js
@@ -18,18 +18,22 @@ var createFingerprintview = function( options, $node ) {
 		value: {
 			language: 'en',
 			label: 'test label',
-			description: 'test description'
+			description: 'test description',
+			aliases: ['alias1', 'alias2']
 		}
 	}, options || {} );
 
-	$node = $node || $( '<div/>' ).appendTo( 'body' );
+	$node = $node || $( '<tbody/>' ).appendTo( $( '<table/>' ) );
 
 	var $fingerprintview = $node
 		.addClass( 'test_fingerprintview' )
 		.fingerprintview( options );
 
-	$fingerprintview.data( 'fingerprintview' ).$label.data( 'labelview' )._save
-		= $fingerprintview.data( 'fingerprintview' ).$description.data( 'descriptionview' )._save
+	var fingerprintview = $fingerprintview.data( 'fingerprintview' );
+
+	fingerprintview.$labelview.data( 'labelview' )._save
+		= fingerprintview.$descriptionview.data( 'descriptionview' )._save
+		= fingerprintview.$aliasesview.data( 'aliasesview' )._save
 		= function() {
 			return $.Deferred().resolve( {
 				entity: {
@@ -171,7 +175,8 @@ QUnit.test( 'value()', function( assert ) {
 	fingerprintview.value( {
 		language: 'en',
 		label: 'changed label',
-		description: 'test description'
+		description: 'test description',
+		aliases: ['alias1', 'alias2']
 	} );
 
 	assert.ok(
@@ -195,7 +200,8 @@ QUnit.test( 'value()', function( assert ) {
 	fingerprintview.value( {
 		language: 'en',
 		label: 'test label',
-		description: null
+		description: null,
+		aliases: ['alias1', 'alias2']
 	} );
 
 	assert.equal(
@@ -210,12 +216,39 @@ QUnit.test( 'value()', function( assert ) {
 		'Removed description.'
 	);
 
+	fingerprintview.value( {
+		language: 'en',
+		label: 'test label',
+		description: null,
+		aliases: ['alias1', 'alias2', 'alias3']
+	} );
+
+	assert.deepEqual(
+		fingerprintview.value().aliases,
+		['alias1', 'alias2', 'alias3'],
+		'Added alias.'
+	);
+
+	fingerprintview.value( {
+		language: 'en',
+		label: 'test label',
+		description: null,
+		aliases: []
+	} );
+
+	assert.deepEqual(
+		fingerprintview.value().aliases,
+		[],
+		'Removed aliases.'
+	);
+
 	assert.throws(
 		function() {
 			fingerprintview.value( {
 				language: 'de',
 				label: 'test label',
-				description: null
+				description: null,
+				aliases: ['alias1', 'alias2']
 			} );
 		},
 		'Trying to change language fails.'