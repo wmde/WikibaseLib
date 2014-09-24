wikimedia/mediawiki-extensions-Wikibase

Refactor EntityStore
Change-Id: If8cfe64020d056fda028c9dc8f275ee6eb4a65c7

lib/tests/qunit/jquery.wikibase/jquery.wikibase.sitelinkgroupview.tests.js
@@ -14,7 +14,7 @@ function createSitelinkgroupview( options ) {
 	options = $.extend( {
 		entityId: 'i am an entity id',
 		api: 'i am an api',
-		entityStore: new wb.store.EntityStore( null )
+		entityStore: new wb.store.CombiningEntityStore()
 	}, options );
 
 	return $( '<div/>' )

lib/tests/qunit/jquery.wikibase/jquery.wikibase.sitelinkview.tests.js
@@ -12,7 +12,7 @@
 	 */
 	function createSitelinkview( options ) {
 		options = $.extend( {
-			entityStore: new wb.store.EntityStore( null ),
+			entityStore: new wb.store.CombiningEntityStore(),
 			allowedSiteIds: ['aawiki', 'enwiki']
 		}, options );
 

wmde/WikibaseLib

tests/qunit/jquery.wikibase/jquery.wikibase.sitelinkgroupview.tests.js
@@ -14,7 +14,7 @@ function createSitelinkgroupview( options ) {
 	options = $.extend( {
 		entityId: 'i am an entity id',
 		api: 'i am an api',
-		entityStore: new wb.store.EntityStore( null )
+		entityStore: new wb.store.EntityStore()
 	}, options );
 
 	var $sitelinkgroupview = $( '<div/>' )

tests/qunit/jquery.wikibase/jquery.wikibase.sitelinkview.tests.js
@@ -12,7 +12,7 @@
 	 */
 	function createSitelinkview( options ) {
 		options = $.extend( {
-			entityStore: new wb.store.EntityStore( null ),
+			entityStore: new wb.store.EntityStore(),
 			allowedSiteIds: ['aawiki', 'enwiki']
 		}, options );
 