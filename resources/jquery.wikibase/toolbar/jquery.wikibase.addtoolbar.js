wikimedia/mediawiki-extensions-Wikibase

Implemented singlebuttontoolbar
Change-Id: I491e216a459276837f6ce095bec17e051a17326b

lib/resources/jquery.wikibase/toolbar/jquery.wikibase.addtoolbar.js
@@ -2,66 +2,29 @@
  * @licence GNU GPL v2+
  * @author H. Snater < mediawiki@snater.com >
  */
-// TODO: Merge with removetoolbar.
 ( function( mw, $ ) {
 	'use strict';
 
-var PARENT = $.wikibase.toolbar;
+var PARENT = $.wikibase.singlebuttontoolbar;
 
 /**
- * "Add" toolbar widget.
+ * "Add" toolbar widget by default offering an "add" button.
  * @since 0.4
- * @extends jQuery.wikibase.toolbar
+ * @extends jQuery.wikibase.singlebuttontoolbar
  *
- * This widget, by default, offers an "add" button.
+ * @option {string} [label]
+ *         Default: mw.msg( 'wikibase-add' )
  *
- * @option {boolean} [renderItemSeparators]
- *         Default: true
- *
- * @event add
- *        Triggered when the default "add" button is hit.
- *        - {jQuery.Event}
+ * @option {string} [eventName]
+ *         Default: 'add'
  */
 $.widget( 'wikibase.addtoolbar', PARENT, {
 	/**
-	 * @see jQuery.wikibase.toolbar.options
+	 * @see jQuery.wikibase.singlebuttontoolbar.options
 	 */
 	options: {
-		renderItemSeparators: true,
-		label: mw.msg( 'wikibase-add' )
-	},
-
-	/**
-	 * @see jQuery.wikibase.toolbar._create
-	 */
-	_create: function() {
-		PARENT.prototype._create.call( this );
-
-		if( !this.options.$content.length ) {
-			this.options.$content = this._createDefaultButton().appendTo( this._getContainer() );
-			this.draw();
-		}
-	},
-
-	/**
-	 * @return {jQuery}
-	 */
-	_createDefaultButton: function() {
-		var self = this;
-
-		return $( '<span/>' ).toolbarbutton( {
-			$label: this.options.label
-		} )
-		.on( 'toolbarbuttonaction.' + this.widgetName, function( event ) {
-			self._trigger( 'add' );
-		} );
-	},
-
-	focus: function() {
-		var button = this.options.$content.first().data( 'toolbarbutton' );
-		if( button ) {
-			button.focus();
-		}
+		label: mw.msg( 'wikibase-add' ),
+		eventName: 'add'
 	}
 } );
 

lib/resources/jquery.wikibase/toolbar/jquery.wikibase.removetoolbar.js
@@ -2,66 +2,29 @@
  * @licence GNU GPL v2+
  * @author H. Snater < mediawiki@snater.com >
  */
-// TODO: Merge with addtoolbar.
 ( function( mw, $ ) {
 	'use strict';
 
-var PARENT = $.wikibase.toolbar;
+var PARENT = $.wikibase.singlebuttontoolbar;
 
 /**
- * "Remove" toolbar widget.
+ * "Remove" toolbar widget by default offering a "remove" button.
  * @since 0.4
- * @extends jQuery.wikibase.toolbar
+ * @extends jQuery.wikibase.singlebuttontoolbar
  *
- * This widget, by default, offers an "remove" button.
+ * @option {string} [label]
+ *         Default: mw.msg( 'wikibase-remove' )
  *
- * @option {boolean} [renderItemSeparators]
- *         Default: true
- *
- * @event remove
- *        Triggered when the default "remove" button is hit.
- *        - {jQuery.Event}
+ * @option {string} [eventName]
+ *         Default: 'remove'
  */
 $.widget( 'wikibase.removetoolbar', PARENT, {
 	/**
-	 * @see jQuery.wikibase.toolbar.options
+	 * @see jQuery.wikibase.singlebuttontoolbar.options
 	 */
 	options: {
-		renderItemSeparators: true,
-		label: mw.msg( 'wikibase-remove' )
-	},
-
-	/**
-	 * @see jQuery.wikibase.toolbar._create
-	 */
-	_create: function() {
-		PARENT.prototype._create.call( this );
-
-		if( !this.options.$content.length ) {
-			this.options.$content = this._createDefaultButton().appendTo( this._getContainer() );
-			this.draw();
-		}
-	},
-
-	/**
-	 * @return {jQuery}
-	 */
-	_createDefaultButton: function() {
-		var self = this;
-
-		return $( '<span/>' ).toolbarbutton( {
-			$label: this.options.label
-		} )
-		.on( 'toolbarbuttonaction.' + this.widgetName, function( event ) {
-			self._trigger( 'remove' );
-		} );
-	},
-
-	focus: function() {
-		var button = this.options.$content.first().data( 'toolbarbutton' );
-		if( button ) {
-			button.focus();
-		}
+		label: mw.msg( 'wikibase-remove' ),
+		eventName: 'remove'
 	}
 } );
 

wmde/WikibaseLib

resources/jquery.wikibase/toolbar/jquery.wikibase.addtoolbar.js
@@ -2,67 +2,30 @@
  * @licence GNU GPL v2+
  * @author H. Snater < mediawiki@snater.com >
  */
-// TODO: Merge with removetoolbar.
 ( function( mw, $ ) {
 	'use strict';
 
-var PARENT = $.wikibase.toolbar;
+var PARENT = $.wikibase.singlebuttontoolbar;
 
 /**
- * "Add" toolbar widget.
- * @extends jQuery.wikibase.toolbar
+ * "Add" toolbar widget by default offering an "add" button.
+ * @extends jQuery.wikibase.singlebuttontoolbar
  * @since 0.4
  *
- * This widget, by default, offers an "add" button.
+ * @option {string} [label]
+ *         Default: mw.msg( 'wikibase-add' )
  *
- * @option {boolean} [renderItemSeparators]
- *         Default: true
- *
- * @event add
- *        Triggered when the default "add" button is hit.
- *        - {jQuery.Event}
+ * @option {string} [eventName]
+ *         Default: 'add'
  */
 $.widget( 'wikibase.addtoolbar', PARENT, {
 	/**
-	 * @see jQuery.wikibase.toolbar.options
+	 * @see jQuery.wikibase.singlebuttontoolbar.options
 	 */
 	options: {
-		renderItemSeparators: true,
-		label: mw.msg( 'wikibase-add' )
-	},
-
-	/**
-	 * @see jQuery.wikibase.toolbar._create
-	 */
-	_create: function() {
-		PARENT.prototype._create.call( this );
-
-		if( !this.options.$content.length ) {
-			this.options.$content = this._createDefaultButton().appendTo( this._getContainer() );
-			this.draw();
-		}
-	},
-
-	/**
-	 * @return {jQuery}
-	 */
-	_createDefaultButton: function() {
-		var self = this;
-
-		return $( '<span/>' ).toolbarbutton( {
-			$label: this.options.label,
-			cssClassSuffix: 'add'
-		} )
-		.on( 'toolbarbuttonaction.' + this.widgetName, function( event ) {
-			self._trigger( 'add' );
-		} );
-	},
-
-	focus: function() {
-		var button = this.options.$content.first().data( 'toolbarbutton' );
-		if( button ) {
-			button.focus();
-		}
+		label: mw.msg( 'wikibase-add' ),
+		eventName: 'add',
+		buttonCssClassSuffix: 'add'
 	}
 } );
 

resources/jquery.wikibase/toolbar/jquery.wikibase.removetoolbar.js
@@ -2,67 +2,30 @@
  * @licence GNU GPL v2+
  * @author H. Snater < mediawiki@snater.com >
  */
-// TODO: Merge with addtoolbar.
 ( function( mw, $ ) {
 	'use strict';
 
-var PARENT = $.wikibase.toolbar;
+var PARENT = $.wikibase.singlebuttontoolbar;
 
 /**
- * "Remove" toolbar widget.
+ * "Remove" toolbar widget by default offering a "remove" button.
  * @since 0.4
- * @extends jQuery.wikibase.toolbar
+ * @extends jQuery.wikibase.singlebuttontoolbar
  *
- * This widget, by default, offers an "remove" button.
+ * @option {string} [label]
+ *         Default: mw.msg( 'wikibase-remove' )
  *
- * @option {boolean} [renderItemSeparators]
- *         Default: true
- *
- * @event remove
- *        Triggered when the default "remove" button is hit.
- *        - {jQuery.Event}
+ * @option {string} [eventName]
+ *         Default: 'remove'
  */
 $.widget( 'wikibase.removetoolbar', PARENT, {
 	/**
-	 * @see jQuery.wikibase.toolbar.options
+	 * @see jQuery.wikibase.singlebuttontoolbar.options
 	 */
 	options: {
-		renderItemSeparators: true,
-		label: mw.msg( 'wikibase-remove' )
-	},
-
-	/**
-	 * @see jQuery.wikibase.toolbar._create
-	 */
-	_create: function() {
-		PARENT.prototype._create.call( this );
-
-		if( !this.options.$content.length ) {
-			this.options.$content = this._createDefaultButton().appendTo( this._getContainer() );
-			this.draw();
-		}
-	},
-
-	/**
-	 * @return {jQuery}
-	 */
-	_createDefaultButton: function() {
-		var self = this;
-
-		return $( '<span/>' ).toolbarbutton( {
-			$label: this.options.label,
-			cssClassSuffix: 'remove'
-		} )
-		.on( 'toolbarbuttonaction.' + this.widgetName, function( event ) {
-			self._trigger( 'remove' );
-		} );
-	},
-
-	focus: function() {
-		var button = this.options.$content.first().data( 'toolbarbutton' );
-		if( button ) {
-			button.focus();
-		}
+		label: mw.msg( 'wikibase-remove' ),
+		eventName: 'remove',
+		buttonCssClassSuffix: 'remove'
 	}
 } );
 